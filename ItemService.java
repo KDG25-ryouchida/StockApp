12099c6841dd9cc7e3ee2ceb39d3891794ba8bca
内田諒 <uchidaryo0328kdz@gmail.com>
2026-09-21 17:33:53 +0900

     ItemService: isRecommendedの適用

--- ItemService.java
+++ ItemService.java
@@ -8,22 +8,7 @@ public class ItemService {
 	private List<Item> itemList = new ArrayList<>();
 	private int nextId = 1;
 
-	// 枠組み
-	public void showAll() {
-		///リストが空の時
-		if (itemList.isEmpty()) {
-			System.out.println("現在登録されているストックはありません");
-		}
-		///リストに登録されたものがある時
-		else {
-			System.out.println("ストック一覧");
-			for (Item item : itemList) {
-				System.out.println("ID" + item.getId() + "名前" + item.getName());
-			}
-		}
-	}
-
-	///データの登録メソッド
+	///データの登録メソッド　番号:1
 	public void addItem(String name, String category, int status, String memo) {
 		///新しいアイテムの作成、リストへの追加
 		Item newItem = new Item(nextId, name, category, status, memo);
@@ -35,4 +20,38 @@ public class ItemService {
 
 	}
 
+	///一覧表示　番号:2
+	///全件表示　番号:2-1
+	public void showAll() {
+		if (itemList.isEmpty()) {
+			System.out.println("現在登録されているストックはありません");
+		} else {
+			System.out.println("ストック一覧");
+			for (Item item : itemList) {
+				System.out.println("ID: " + item.getId() + "名前: " + item.getName() + "カテゴリ: " + item.getCategory()
+						+ "残量: " + item.getStatus() + "メモ: " + item.getMemo());
+			}
+		}
+	}
+
+	///購入品のレコメンド機能（残量少、無し）　番号2-2
+	public void showRecommendItem() {
+		if (itemList.isEmpty()) {
+			System.out.println("現在登録されているストックはありません");
+			return;
+		}
+
+		boolean found = false;
+		System.out.println("購入品リスト");
+		for (Item item : itemList) {
+			if ((item.isRecommended())) {
+				System.out.println("ID: " + item.getId() + "名前: " + item.getName() + "残量: " + item.getStatus());
+				found = true;
+			}
+		}
+		if (!found) {
+			System.out.println("現在、購入が必要なストックはありません。");
+
+		}
+	}
 }
